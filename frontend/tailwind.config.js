f48c1a7531dc977c12395a9856673bfe6a39ccad
Matias Zuriel <matiasaquinozc@gmail.com>
2025-12-02 15:38:25 -0500

    feat: add colors and typografy

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,10 @@ export default {
             colors: {
                 'brand-dark': '#0b1736',
                 'brand-orange': '#ee6123',
+                'text-primary': '#FFFDF9',
+                'text-secondary': '#EF6123',
+                'text-tertiary': '#878784',
+                'background-primary': '#021F39',
             }
         },
     },
